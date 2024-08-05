82a6f9faefa7217d9c6fda7a1c4297e06e1667f1
D Mitra <dmitra088@gmail.com>
2024-08-05 23:35:07 +0530

    add test cases extended

--- index.test.js
+++ index.test.js
@@ -13,3 +13,18 @@ test('should return the sum of two numbers separated by a comma', () => {
     expect(add("1,5")).toBe(6);
     expect(add("10,20")).toBe(30);
 });
+
+test('should handle new lines between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+    expect(add("4\n5\n6")).toBe(15);
+});
+
+test('should handle custom delimiters specified at the start of the string', () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//|\n3|4|5")).toBe(12);
+});
+
+test('should throw an error if negative numbers are included', () => {
+    expect(() => add("1,-2,3")).toThrow("negative numbers not allowed -2");
+    expect(() => add("//;\n-1;2;-3")).toThrow("negative numbers not allowed -1,-3");
+});
