f312c015ed4776a04ed783c67cd976138c9e27d6
D Mitra <dmitra088@gmail.com>
2024-08-05 23:24:34 +0530

    add test cases extended

--- index.test.js
+++ index.test.js
@@ -3,3 +3,13 @@ const add = require('./index');
 test('should return 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('should return the number itself for a single number', () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+});
+
+test('should return the sum of two numbers separated by a comma', () => {
+    expect(add("1,5")).toBe(6);
+    expect(add("10,20")).toBe(30);
+});
